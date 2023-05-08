dc0a2d3e2fd3da589c3142617e7f7d0ecae1cdde
treezyxd <thalesmaltaxd@gmail.com>
2023-05-08 11:54:45 -0300

    atlas

--- src/controllers/choiceController.js
+++ src/controllers/choiceController.js
@@ -5,13 +5,13 @@ import dayjs from "dayjs";
 export async function postVote (req,res){
   const { id } = req.params;
   let currentDate = dayjs();
-  let date= currentDate.format("YYYY-MM-DD HH:mm");
-  let choice= await db.collection("choices").findOne({_id:  ObjectId(id)});
+  let date = currentDate.format("YYYY-MM-DD HH:mm");
+  let choice = await db.collection("choices").findOne({_id:  ObjectId(id)});
   if (choice) {
       let poll = await db.collection("polls").findOne({_id:  ObjectId(choice.pollId)});
       const timestamp = dayjs(poll.expireAt).toDate().getTime();
       if (poll && currentDate < timestamp){
-          try{
+          try {
             let vote = await db.collection("votes").insertOne({createdAt: date, choiceId:  ObjectId(id)});
             return res.sendStatus(201).send(vote);
           } catch(err){
@@ -19,59 +19,57 @@ export async function postVote (req,res){
           }
       }
       else{
-          return res.sendStatus(403)
+        return res.sendStatus(403);
       }
   }
   else{
-      return res.sendStatus(404)
+    return res.sendStatus(404);
   }
 }
 
-export async function getChoices( req, res) {
-  const {id}= req.params;
-  let poll= await db.collection("polls").findOne({_id: ObjectId(id)})
-  if (!poll){
-      return res.sendStatus(404)
+export async function getChoices(req, res) {
+  const { id } = req.params;
+  let poll = await db.collection("polls").findOne({_id: ObjectId(id)});
+  if (!poll) {
+    ;
   }
   else{
-      try{
-          let choices= await db.collection("choices").find({pollId :  ObjectId(id)}).toArray();
-          return res.status(200).send(choices);
-      } catch(err){
-        console.log(err.message);
-      }
+    try {
+      let choices= await db.collection("choices").find({pollId :  ObjectId(id)}).toArray();
+      return res.status(200).send(choices);
+    } catch(err){
+      console.log(err.message);
+    }
   }
 }
 
-export async function postChoice(req,res){
-  const { title, pollId }= req.body;
+export async function postChoice(req,res) {
+  const { title, pollId } = req.body;
   let currentDate = dayjs().toDate().getTime();
 
   let poll = await db.collection("polls").findOne({_id:  ObjectId(pollId)});
 
 
-  if(!poll){
-      return res.sendStatus(404);
+  if(!poll) {
+    return res.sendStatus(404);
   }
-  else{
-      const timestamp = dayjs(poll.expireAt).toDate().getTime()
-      let name = await db.collection("choices").findOne({title: title});
-      if(name){
-      return res.sendStatus(409);
+  else {
+    const timestamp = dayjs(poll.expireAt).toDate().getTime();
+    let name = await db.collection("choices").findOne({title: title});
+    if(name) {
+    return res.sendStatus(409);
+    }
+    else if(currentDate > timestamp) {
+      return res.sendStatus(403);
+    }
+    else {
+      try {
+        await db.collection("choices").insertOne({title: title, pollId: ObjectId(pollId)});
+        let choice= await db.collection("choices").findOne({title: title});
+        return res.status(201).send(choice);
+      } catch(err) {
+          console.log(err.message);
       }
-      else if(currentDate > timestamp) {
-        return res.sendStatus(403);
-      }
-      else {
-        try {
-          await db.collection("choices").insertOne({title: title, pollId: ObjectId(pollId)});
-          let choice= await db.collection("choices").findOne({title: title});
-          return res.status(201).send(choice);
-        } catch(err) {
-            console.log(err.message);
-        }
-      }
-
+    }
   }
-
 }
